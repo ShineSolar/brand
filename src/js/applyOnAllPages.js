1b7f0ec6488168d7a1f03039a4cf1ec411aac890
Adam McGurk <mcgurk@mcgurkconsulting.com>
2019-01-24 11:58:22 -0700

    Deleted the apply on all pages. I want the authors to have more control

--- src/js/applyOnAllPages.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener('load', function() {
-	if (document.querySelector('.loading-animation') !== null) {
-		document.querySelector('.loading-animation').classList.add('loaded');
-	}
-})
-
